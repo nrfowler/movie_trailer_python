05405daa403437c142050a97077c522f4e9741ce
nrfowler <nrfowler@gmail.com>
2016-12-03 12:32:27 -0600

    commented media.py

--- media.py
+++ media.py
@@ -1,4 +1,15 @@
 class Movie():
+    """
+    Args:
+        title (str): String containing the title of the film.
+        trailer_youtube_url (str): String containing the youtube url of the trailer.
+        poster_image_url (str): String containing the image url of the film poster.
+
+    Attributes:
+        title (str): String containing the title of the film.
+        trailer_youtube_url (str): String containing the youtube url of the trailer.
+        poster_image_url (str): String containing the image url of the film poster.
+    """
     def __init__(self, title, trailer_youtube_url, poster_image_url):
         self.title=title
         self.poster_image_url=poster_image_url
